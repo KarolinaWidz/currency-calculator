864a54df376a0050381502e52de98cfdadbe0c53
Karolina Widz <karolinawidz97@gmail.com>
2021-10-14 20:15:42 +0200

    Initial Commit

---
+++ src/test/java/edu/karolinawidz/currencycalculator/CurrencyCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.karolinawidz.currencycalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurrencyCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
